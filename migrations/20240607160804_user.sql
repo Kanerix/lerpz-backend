3948a2d93a9c52b0317a3048e83aacc4ec803942
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-06-08 16:53:58 +0200

    Added migrations

---
+++ migrations/20240607160804_user.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE role AS ENUM ('user', 'moderator', 'admin');
+
+CREATE TABLE users (
+	id 			    uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username        VARCHAR(32) NOT NULL UNIQUE,
+    email           VARCHAR(255) NOT NULL UNIQUE,
+    password_hash   VARCHAR(255) NOT NULL UNIQUE,
+    updated_at      TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
